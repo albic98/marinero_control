74510b4bb00f0567cb4aa8aada9ff487029e6898
albic98 <albert.androsic2@gmail.com>
2025-03-06 15:06:24 +0100

    Update 06_03_1

--- marinero_control/marinero_camera_w_joy.py
+++ marinero_control/marinero_camera_w_joy.py
@@ -4,88 +4,57 @@ import rclpy
 import math
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from rclpy.node import Node
-from std_msgs.msg import Header
 from sensor_msgs.msg import Joy
-from pynput import keyboard
 
 class Camera(Node):
     def __init__(self):
         super().__init__("camera")
-        self.camera_pose_publisher = self.create_publisher(JointTrajectory, "/set_joint_trajectory", 10)
+        self.camera_trajectory_publisher = self.create_publisher(JointTrajectory, "/joint_trajectory_controller/joint_trajectory", 10)
         self.joy_subscriber = self.create_subscription(Joy, "/joy", self.joy_callback, 10)
-        self.cam_base = 0.0   # Initial value for cam_base
-        self.l_cam = 1.58      # Initial value for l_cam
-        self.r_cam = 1.58     # Initial value for r_cam
-        self.cam_base_position = 0.0
-        self.counter = 0.0
+
+        self.joint_names = ["camera_base_joint", "left_camera_joint", "right_camera_joint"]
+        self.positions = [0.0, 1.5, 1.5]  # Initial positions
         self.axis_flag = Joy()
         self.axis_flag.axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        self.camera_initialized = False
-        self.joint_names = ["camera_base_joint", "left_camera_joint", "right_camera_joint"]
-        self.last_button_press = 0.0
-        self.button_delay = 0.2  # 200 ms delay to avoid rapid button presses
-        self.create_timer(0.1, self.joint_pose_publisher)
-
+                          
     def joy_callback(self, msg):
         self.axis_flag = msg
-        now = self.get_clock().now().seconds_nanoseconds()[0]
 
-        if msg.axes[6] == 1:                   # LEFT
-            self.adjust_joint("cam_base", -0.05)
-        elif msg.axes[6] == -1:                 # RIGHT
-            self.adjust_joint("cam_base", 0.05)
-        elif msg.axes[7] == 1:                 # UP
-            self.adjust_joint("l_cam", -0.05)
-            self.adjust_joint("r_cam", -0.05)
-        elif msg.axes[7] == -1:                 # DOWN 
-            self.adjust_joint("l_cam", 0.05)
-            self.adjust_joint("r_cam", 0.05)
-        else:
-            self.adjust_joint("cam_base", 0.0)
-            self.adjust_joint("l_cam", 0.0)
-            self.adjust_joint("r_cam", 0.0)
-
-        if now - self.last_button_press < self.button_delay:
-            return  # Prevent repeated command execution due to rapid button presses
-
-    def adjust_joint(self, joint, delta):
-
-        if joint == "cam_base":
-            self.cam_base = max(min(self.cam_base + delta, math.pi), -math.pi)
-        elif joint == "l_cam":
-            self.l_cam = max(min(self.l_cam + delta, 2.0), 1.0)
-        elif joint == "r_cam":
-            self.r_cam = max(min(self.r_cam + delta, 2.0), 1.0)
-        # self.get_logger().info(f"Adjusted {joint} to {getattr(self, joint)}")
-
-    def joint_pose_publisher(self):
+        if msg.axes[6] == 1:   # LEFT
+            self.positions[0] = max(min(self.positions[0] - 0.05, math.pi), -math.pi)
+        elif msg.axes[6] == -1:  # RIGHT
+            self.positions[0] = max(min(self.positions[0] + 0.05, math.pi), -math.pi)
         
+        if msg.axes[7] == 1:   # UP
+            self.positions[1] = max(min(self.positions[1] - 0.05, 2.0), 1.0)
+            self.positions[2] = max(min(self.positions[2] - 0.05, 2.0), 1.0)
+        elif msg.axes[7] == -1:  # DOWN
+            self.positions[1] = max(min(self.positions[1] + 0.05, 2.0), 1.0)
+            self.positions[2] = max(min(self.positions[2] + 0.05, 2.0), 1.0)
+        self.send_trajectory()
+
+    def send_trajectory(self):
         msg = JointTrajectory()
-        msg.header = Header()
-        msg.header.frame_id = "base_link"
         msg.joint_names = self.joint_names
+
         point = JointTrajectoryPoint()
-        point.positions = [self.cam_base, self.l_cam, self.r_cam]
+        point.positions = self.positions
         msg.points = [point]
 
-        condition_0 = self.axis_flag.axes[6] != 0
-        condition_1 = self.axis_flag.axes[7] != 0
-        
-        if condition_0 or condition_1:
-            self.camera_pose_publisher.publish(msg)
-            self.counter = 0.0
-        elif self.counter > 30.0:
-            self.camera_pose_publisher.publish(msg)
-            self.counter = 0.0
+        condition_0 = self.axis_flag.axes[6] == 0
+        condition_1 = self.axis_flag.axes[7] == 0
+
+        if condition_0 and condition_1:
+            return
         else:
-            self.counter += 0.1
+            self.camera_trajectory_publisher.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
     camera_node = Camera()
     rclpy.spin(camera_node)
     camera_node.destroy_node()
-    rclpy.shutdown()   
+    rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
--- marinero_control/marinero_control_with_autonomy.py
+++ marinero_control/marinero_control_with_autonomy.py
@@ -5,10 +5,10 @@ import rclpy
 import time
 import numpy as np
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, TwistStamped
 from std_msgs.msg import Float64MultiArray, Int32
 
-vel_msg = Twist()       # robot velocity after twist mux node
+vel_msg = TwistStamped()       # robot velocity after twist mux node
 
 class MarineroControl(Node):
 
@@ -40,7 +40,7 @@ class MarineroControl(Node):
         self.target_pos = np.array([0,0,0,0], float)            # target positions for the wheels when the robot is standing still
         self.max_axle_position = math.pi/4                      # radians
 
-        self.final_vel_subscription = self.create_subscription(Twist, "/cmd_vel_out", self.velocity_callback, 10)
+        self.final_vel_subscription = self.create_subscription(TwistStamped, "/cmd_vel_stamped", self.velocity_callback, 10)
         self.camera_subscription = self.create_subscription(Float64MultiArray, "/camera_position", self.camera_pose_callback, 10)  
         self.mode_selection_subscription = self.create_subscription(Int32, "/mode_selection", self.mode_selection_callback, 10)
         self.pub_pos = self.create_publisher(Float64MultiArray, "/forward_position_controller/commands", 10)
@@ -102,11 +102,11 @@ class MarineroControl(Node):
 
 
     def autonomy_control(self, vel_msg):
-        nav_vel_x = vel_msg.linear.x
-        nav_vel_z = vel_msg.angular.z
-        vel_msg.linear.x = vel_msg.linear.x * self.scale_linear_x
-        vel_msg.linear.y = vel_msg.linear.y * self.scale_linear_y
-        vel_msg.angular.z = vel_msg.angular.z * self.scale_angular_z
+        nav_vel_x = vel_msg.twist.linear.x
+        nav_vel_z = vel_msg.twist.angular.z
+        vel_msg.twist.linear.x = vel_msg.twist.linear.x * self.scale_linear_x
+        vel_msg.twist.linear.y = vel_msg.twist.linear.y * self.scale_linear_y
+        vel_msg.twist.angular.z = vel_msg.twist.angular.z * self.scale_angular_z
         if nav_vel_z != 0.0 and nav_vel_x == 0.0 or abs(nav_vel_z) > 0.75:
             self.pivot_turn(vel_msg)
         else:
@@ -115,48 +115,48 @@ class MarineroControl(Node):
 
     def in_phase_steering(self, vel_msg):
         if self.initial_sign_x == 0.0 or self.entered_function_flag == False: 
-            self.initial_sign_x = np.sign(vel_msg.linear.x)
+            self.initial_sign_x = np.sign(vel_msg.twist.linear.x)
             self.entered_function_flag = True
 
         sign_x = self.initial_sign_x
-        sign_y = np.sign(vel_msg.linear.y)
+        sign_y = np.sign(vel_msg.twist.linear.y)
 
-        if np.sign(vel_msg.linear.x) != self.initial_sign_x:
-            vel_msg.linear.x = 0.0
-            vel_msg.linear.y = math.copysign(self.scale_linear_y, sign_y)
+        if np.sign(vel_msg.twist.linear.x) != self.initial_sign_x:
+            vel_msg.twist.linear.x = 0.0
+            vel_msg.twist.linear.y = math.copysign(self.scale_linear_y, sign_y)
             axle_angle = self.previous_angle
-        elif vel_msg.linear.x != 0:
-            axle_angle = math.atan(vel_msg.linear.y / vel_msg.linear.x)
+        elif vel_msg.twist.linear.x != 0:
+            axle_angle = math.atan(vel_msg.twist.linear.y / vel_msg.twist.linear.x)
             self.previous_angle = axle_angle
         else:
             axle_angle = 0.0
 
-        V = math.hypot(vel_msg.linear.x, vel_msg.linear.y)  # Combined linear velocity magnitude
+        V = math.hypot(vel_msg.twist.linear.x, vel_msg.twist.linear.y)  # Combined linear velocity magnitude
         self.pos[:4] = axle_angle
         self.vel[:] = sign_x * V
 
 
     def opposite_phase_steering(self, vel_msg):
-        vel_steerring_offset = vel_msg.angular.z * self.wheel_steering_y_offset
-        sign = np.sign(vel_msg.linear.x)
+        vel_steerring_offset = vel_msg.twist.angular.z * self.wheel_steering_y_offset
+        sign = np.sign(vel_msg.twist.linear.x)
 
-        self.vel[0] = sign*math.hypot(vel_msg.linear.x - vel_msg.angular.z*self.steering_track/2, vel_msg.angular.z*self.wheel_base/2) - vel_steerring_offset
-        self.vel[1] = sign*math.hypot(vel_msg.linear.x + vel_msg.angular.z*self.steering_track/2, vel_msg.angular.z*self.wheel_base/2) + vel_steerring_offset
-        self.vel[2] = sign*math.hypot(vel_msg.linear.x - vel_msg.angular.z*self.steering_track/2, vel_msg.angular.z*self.wheel_base/2) - vel_steerring_offset
-        self.vel[3] = sign*math.hypot(vel_msg.linear.x + vel_msg.angular.z*self.steering_track/2, vel_msg.angular.z*self.wheel_base/2) + vel_steerring_offset
+        self.vel[0] = sign*math.hypot(vel_msg.twist.linear.x - vel_msg.twist.angular.z*self.steering_track/2, vel_msg.twist.angular.z*self.wheel_base/2) - vel_steerring_offset
+        self.vel[1] = sign*math.hypot(vel_msg.twist.linear.x + vel_msg.twist.angular.z*self.steering_track/2, vel_msg.twist.angular.z*self.wheel_base/2) + vel_steerring_offset
+        self.vel[2] = sign*math.hypot(vel_msg.twist.linear.x - vel_msg.twist.angular.z*self.steering_track/2, vel_msg.twist.angular.z*self.wheel_base/2) - vel_steerring_offset
+        self.vel[3] = sign*math.hypot(vel_msg.twist.linear.x + vel_msg.twist.angular.z*self.steering_track/2, vel_msg.twist.angular.z*self.wheel_base/2) + vel_steerring_offset
 
-        if vel_msg.linear.x == 0.0:
+        if vel_msg.twist.linear.x == 0.0:
             self.target_pos[0] = 0.0
             self.target_pos[1] = 0.0
             self.target_pos[2] = 0.0
             self.target_pos[3] = 0.0
-        elif vel_msg.angular.z < 0:
-            self.pos[0] = -1.5*math.atan(vel_msg.angular.z*self.wheel_base/(2*abs(vel_msg.linear.x) + vel_msg.angular.z*self.steering_track))
+        elif vel_msg.twist.angular.z < 0:
+            self.pos[0] = -1.5*math.atan(vel_msg.twist.angular.z*self.wheel_base/(2*abs(vel_msg.twist.linear.x) + vel_msg.twist.angular.z*self.steering_track))
             if abs(self.pos[0]) > self.max_axle_position:
                 self.pos[0] = self.max_axle_position
             self.axle_positions()
         else:
-            self.pos[0] = -1.5*math.atan(vel_msg.angular.z*self.wheel_base/(2*abs(vel_msg.linear.x) - vel_msg.angular.z*self.steering_track))
+            self.pos[0] = -1.5*math.atan(vel_msg.twist.angular.z*self.wheel_base/(2*abs(vel_msg.twist.linear.x) - vel_msg.twist.angular.z*self.steering_track))
             if abs(self.pos[0]) > self.max_axle_position:
                 self.pos[0] = -self.max_axle_position
             self.axle_positions()
@@ -168,8 +168,8 @@ class MarineroControl(Node):
         self.target_pos[3] = math.atan(self.wheel_base / self.steering_track)
 
         if self.target_pos[0] == self.pos[0]:
-            self.vel[0] = - vel_msg.angular.z
-            self.vel[1] = vel_msg.angular.z
+            self.vel[0] = - vel_msg.twist.angular.z
+            self.vel[1] = vel_msg.twist.angular.z
             self.vel[2] = self.vel[0]
             self.vel[3] = self.vel[1]
         else:
